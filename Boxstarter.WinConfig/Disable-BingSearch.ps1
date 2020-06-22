7c2c458056a755a7af70368eda3a12c7d0e52d8b
Joseph Musser <me@jnm2.com>
2020-06-22 13:59:02 -0400

    Update Disable-BingSearch for Windows 10 2004
    The registry key changes in update 2004.
    https://www.lifehacker.com.au/2020/06/how-to-disable-bing-search-in-windows-10s-start-menu-2/

--- Boxstarter.WinConfig/Disable-BingSearch.ps1
+++ Boxstarter.WinConfig/Disable-BingSearch.ps1
@@ -6,6 +6,7 @@ Disables the Bing Internet Search when searching from the search field in the Ta
 .LINK
 https://boxstarter.org
 https://www.privateinternetaccess.com/forum/discussion/18301/how-to-uninstall-core-apps-in-windows-10-and-miscellaneous
+https://www.lifehacker.com.au/2020/06/how-to-disable-bing-search-in-windows-10s-start-menu-2/
 
 #>
     $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
@@ -16,4 +17,8 @@ https://www.privateinternetaccess.com/forum/discussion/18301/how-to-uninstall-co
 
     New-ItemProperty -LiteralPath $path -Name "BingSearchEnabled" -Value 0 -PropertyType "DWord" -ErrorAction SilentlyContinue
     Set-ItemProperty -LiteralPath $path -Name "BingSearchEnabled" -Value 0
+    
+    $2004path = 'HKCU:\Software\Policies\Microsoft\Windows\Explorer'
+    if (!(Test-Path $2004path)) { New-Item $2004path }
+    Set-ItemProperty $2004path DisableSearchBoxSuggestions 1
 }
